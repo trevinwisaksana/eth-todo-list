1fc2aa5541bab82d16021f621a0a4c77c9da026f
Trevin Wisaksana <trevinwisaksana@gmail.com>
2021-05-04 17:54:59 +0800

    Added the ability to show first task and added tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,20 @@ pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount = 0;
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out my new todolist");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
